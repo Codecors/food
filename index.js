bf859828db0aeceab4f4e6d28d4c4a34169c255d
Codecors <info@stylehairs.tk>
2016-07-07 22:36:03 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 /*
     * LOVELY THINGS
     */
-var list = document.getElementById('list').querySelectorAll('li');
+      var list = document.getElementById('list').querySelectorAll('li');
 
        Array.prototype.slice.call(list).forEach(function(listItem){
           listItem.addEventListener('click', function(e){
           document.getElementById('overlays').style="display:block";  
           var y = this.id;
+          var font = document.getElementById(y).querySelectorAll('font');
           var hdr = document.getElementsByClassName('name');
           var phn = document.getElementsByClassName('phne');
           var adr = document.getElementsByClassName('adres');
           var bdg = document.getElementsByClassName('budget');
           var crd = document.getElementsByClassName('credit');
           var atr = document.getElementsByClassName('host');
-          document.getElementById("ovrhdr").innerHTML = hdr[y].innerHTML;
-          document.getElementById("ovrphn").innerHTML = phn[y].innerHTML;
-          document.getElementById("ovradr").innerHTML = adr[y].innerHTML;
-          document.getElementById("ovrbdg").innerHTML = bdg[y].innerHTML;
-          document.getElementById("ovrcrd").innerHTML = crd[y].innerHTML;
-          document.getElementById("ovratr").innerHTML = atr[y].innerHTML;
+          console.log(y);
+          /*document.getElementById("ovrhdr").innerHTML = hdr[y].innerHTML;  */
+          document.getElementById("ovrhdr").innerHTML = (document.getElementById(y).querySelector(' .name').innerHTML);
+          document.getElementById("ovrphn").innerHTML = (document.getElementById(y).querySelector(' .phne').innerHTML);
+          document.getElementById("ovradr").innerHTML = (document.getElementById(y).querySelector(' .adres').innerHTML);
+          document.getElementById("ovrbdg").innerHTML = (document.getElementById(y).querySelector(' .budget').innerHTML);
+          document.getElementById("ovrcrd").innerHTML = (document.getElementById(y).querySelector(' .credit').innerHTML);
+          document.getElementById("ovratr").innerHTML = (document.getElementById(y).querySelector(' .host').innerHTML);
           });
         });
 
